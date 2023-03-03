3880d31ca4695fb4b799b2726c75f19a459b9b32
ritakool <jokernguyen1994@gmail.com>
2023-03-03 14:34:38 +0700

    'donethuchanhmangez'

---
+++ main.js
@@ -0,0 +1,20 @@
+let x=0;
+let array=[];
+function add_element_to_array(){
+    array[x] = document.getElementById("txtValue").value;
+    alert("Element: " + array[x] + " Added at index " + x);
+    x++;
+    document.getElementById("txtValue").value = "";
+}
+function display_array()
+{
+    let e = "<hr/>";
+        for (let i=0; i<array.length;i++)
+        {
+            e += "Element " + i + "= " + array[i] + "<br/>";
+        }
+    document.getElementById("result").innerHTML = e;
+}
+// let a = 1;
+// p = a++ + a++;
+// console.log(p);
